ded149768d3aba059f1572e191f52d8d102ffc74
eire44 <eireduo44@gmail.com>
2026-01-27 18:12:51 -0300

    read paper sun bul

---
+++ Escape The Temple/Assets/Scripts/UI/readText.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class readText : MonoBehaviour
+{
+    public Camera playerCamera;
+    public float distance = 4f;
+    public TMP_Text textoTranscripcion;
+
+    void Update()
+    {
+        Ray ray = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
+        RaycastHit hit;
+
+        //Debug.DrawRay(playerCamera.transform.position,
+        //      playerCamera.transform.forward * distance,
+        //      Color.red);
+
+        if (Physics.Raycast(ray, out hit, distance))
+        {
+            if (hit.collider.name.Contains("Papel"))
+            {
+                mostrarTexto("Those who attain enlightenment continue to shine beyond time.");
+            } else
+            {
+                textoTranscripcion.gameObject.SetActive(false);
+            }
+        }
+        else
+        {
+            textoTranscripcion.gameObject.SetActive(false);
+        }
+    }
+
+    void mostrarTexto(string mensaje)
+    {
+        textoTranscripcion.text = mensaje;
+        textoTranscripcion.gameObject.SetActive(true);
+    }
+}
