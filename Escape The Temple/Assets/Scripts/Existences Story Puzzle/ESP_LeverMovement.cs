bb9fff8680a7b1e8b2c4712d6adba554dc3a6952
eire44 <eireduo44@gmail.com>
2026-03-03 14:48:09 -0300

    sagebrush incense color correction

--- Escape The Temple/Assets/Scripts/Existences Story Puzzle/ESP_LeverMovement.cs
+++ Escape The Temple/Assets/Scripts/Existences Story Puzzle/ESP_LeverMovement.cs
@@ -68,7 +68,7 @@ public class ESP_LeverMovement : MonoBehaviour
             gameObject.layer = LayerMask.NameToLayer("Default");
             center.gameObject.layer = LayerMask.NameToLayer("Default");
             FindObjectOfType<ESP_Controller>().enablePuzzle = true;
-            FindObjectOfType<ESP_AudioClueController>().playAudioClue();
+            //FindObjectOfType<ESP_AudioClueController>().playAudioClue();
         }
     }
 }
