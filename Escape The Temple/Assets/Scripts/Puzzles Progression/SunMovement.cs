1d51bf869b612597273a5be6b2d08040f67aa78b
eire44 <eireduo44@gmail.com>
2026-03-13 00:23:43 -0300

    sun progression

---
+++ Escape The Temple/Assets/Scripts/Puzzles Progression/SunMovement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunMovement : MonoBehaviour
+{
+    public Light directionalLight;
+
+    public int totalPuzzles = 10;
+    private int progressionIndex = 0;
+
+    public float startAngle = 90f;
+    public float endAngle = 20f;
+
+    public float startIntensity = 1f;
+    public float endIntensity = 0.5f;
+
+    public Color startColor = Color.white;
+    public Color endColor = new Color(1f, 0.82f, 0.14f);
+
+    float time = 0f;
+    float timeIndex = 10f;
+    private void Update()
+    {
+        //time += Time.deltaTime;
+        //if (time >= timeIndex)
+        //{
+        //    timeIndex += 10f;
+        //    sunProgression();
+        //}
+    }
+
+    public void sunProgression()
+    {
+        if (progressionIndex >= totalPuzzles)
+            return;
+
+        progressionIndex++;
+
+        float t = (float)progressionIndex / totalPuzzles;
+
+        float angle = Mathf.Lerp(startAngle, endAngle, t);
+        directionalLight.transform.rotation = Quaternion.Euler(angle, 0f, 0f);
+        directionalLight.intensity = Mathf.Lerp(startIntensity, endIntensity, t);
+        directionalLight.color = Color.Lerp(startColor, endColor, t);
+    }
+}
+
